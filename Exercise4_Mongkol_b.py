d5279cb8ce4d7a73b304da31c974697cba08978f
khaun <52350325+khaun79@users.noreply.github.com>
2019-08-04 00:03:42 +0700

    Add files via upload

--- Exercise4_Mongkol_b.py
+++ Exercise4_Mongkol_b.py
@@ -1,13 +1,9 @@
-a="Foundation English:"
-FE=60
-b="General Business:"
-GB=65
-c="Introduction to Computer systems:"
-ICS=70
-d="Computer Programming:"
-CR=75
-print("----Your Score---")
-print(a,FE)
-print(b,GB)
-print(c,ICS)
-print(d,CR)
+fE = 60
+gB = 65
+iCS = 70
+cR = 75
+print("------Your Score------")
+print("Foundation English :" + str(fE))
+print("General Business :" + str(gB))
+print("Introduction to Computer systems:" + str(iCS))
+print("Computer Programming:" + str(cR))
